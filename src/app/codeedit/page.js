dd3af14af7b08d79743974f85e5c66cc9c5a2292
Basha <110662505+Bashamega@users.noreply.github.com>
2023-06-24 17:59:34 +0300

    Update CodeEditor - Big Update

--- src/app/codeedit/page.js
+++ src/app/codeedit/page.js
@@ -1,49 +1,55 @@
 "use client";
-import React, { useState } from 'react';
+
+import React, { useState, useEffect, useRef } from 'react';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 import Search from '../assets/search';
+
 export default function Nav() {
-  const [htmlCode, setHtmlCode] = useState('');
+  const [htmlCode, setHtmlCode] = useState(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+  `);
   const [cssCode, setCssCode] = useState('');
   const [jsCode, setJsCode] = useState('');
-  const [result, setResult] = useState('');
+  const [showViewSection, setShowViewSection] = useState(false);
+  const [showIndexHtmlSection, setShowIndexHtmlSection] = useState(false);
+  const [showStyleCssSection, setShowStyleCssSection] = useState(false);
+  const [showScriptJsSection, setShowScriptJsSection] = useState(false);
+  const [code, setCode] = useState('');
+
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    setCode(`
+      <style>${cssCode}</style>
+      ${htmlCode}
+      <script>${jsCode}</script>
+    `);
+  }, [htmlCode, cssCode, jsCode]);
 
   const handleHtmlChange = (event) => {
     const newHtmlCode = event.target.value;
     setHtmlCode(newHtmlCode);
-    executeCode(newHtmlCode, cssCode, jsCode);
   };
 
   const handleCssChange = (event) => {
     const newCssCode = event.target.value;
     setCssCode(newCssCode);
-    executeCode(htmlCode, newCssCode, jsCode);
   };
 
   const handleJsChange = (event) => {
     const newJsCode = event.target.value;
     setJsCode(newJsCode);
-    executeCode(htmlCode, cssCode, newJsCode);
-  };
-
-  const executeCode = (html, css, js) => {
-    const iframe = document.getElementById('result-iframe').contentWindow.document;
-    iframe.open();
-    iframe.writeln(
-      `
-        <html>
-          <head>
-            <style>${css}</style>
-          </head>
-          <body>
-            ${html}
-            <script>${js}</script>
-          </body>
-        </html>
-      `
-    );
-    iframe.close();
   };
 
   const handleDownload = () => {
@@ -65,57 +71,115 @@ export default function Nav() {
     });
   };
 
+  const handleViewClick = () => {
+    setShowViewSection(true);
+    setShowIndexHtmlSection(false);
+    setShowStyleCssSection(false);
+    setShowScriptJsSection(false);
+  };
+
+  useEffect(() => {
+    if (showViewSection) {
+      const iframe = iframeRef.current;
+      iframe.contentWindow.document.open();
+      iframe.contentWindow.document.writeln(code);
+      iframe.contentWindow.document.close();
+    }
+  }, [showViewSection, code]);
+
+  const handleIndexHtmlClick = () => {
+    setShowViewSection(false);
+    setShowIndexHtmlSection(true);
+    setShowStyleCssSection(false);
+    setShowScriptJsSection(false);
+  };
+
+  const handleStyleCssClick = () => {
+    setShowViewSection(false);
+    setShowIndexHtmlSection(false);
+    setShowStyleCssSection(true);
+    setShowScriptJsSection(false);
+  };
+
+  const handleScriptJsClick = () => {
+    setShowViewSection(false);
+    setShowIndexHtmlSection(false);
+    setShowStyleCssSection(false);
+    setShowScriptJsSection(true);
+  };
+  useEffect(() => {
+    document.querySelectorAll('textarea').forEach((textarea) => {
+      textarea.addEventListener('keydown', function(e) {
+        if (e.key === 'Tab') {
+          e.preventDefault();
+
+          // Get the current cursor position
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+
+          // Insert three spaces at the cursor position
+          this.value = this.value.substring(0, start) + '   ' + this.value.substring(end);
+
+          // Move the cursor position after the inserted spaces
+          this.selectionStart = this.selectionEnd = start + 3;
+        }
+      });
+    });
+  }, []);
   return (
-    <div className='h-screen'>
-      <nav className="bg-blue-500 py-4 px-6 flex h-15">
-        <a href='https://web-dev-tools.vercel.app/' class='w-1/5 mr-2 flex border rounded p-2 hover:bg-blue-600'>
-          
+    <div className='h-screen overflow-hidden'>
+      <nav className="bg-blue-500 py-4 px-6 flex">
+        <a href='https://web-dev-tools.vercel.app/' className='w-1/5 mr-2 flex border rounded p-2 hover:bg-blue-600'>
           <h1 className="text-white text-2xl font-bold mr-1">Web Dev Tools</h1>
           <p>Code editor</p>
-          
         </a>
-        
-        <Search/>
-        <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 ml-96"
-          onClick={handleDownload}
-        >
-          Download
-        </button>
+        <Search />
       </nav>
-      <section className="flex h-1/4">
-        <div className="w-1/3 h-full bottom-2 border border-gray-200">
-          <textarea
-            className="w-full h-full outline-none resize-none bg-slate-700"
-            placeholder="HTML"
-            onChange={handleHtmlChange}
-            value={htmlCode}
-          ></textarea>
-        </div>
-        <div className="w-1/3 h-full border border-gray-200 text-black">
-          <textarea
-            className="w-full h-full outline-none resize-none text-black bg-slate-700"
-            placeholder="CSS"
-            onChange={handleCssChange}
-            value={cssCode}
-          ></textarea>
-        </div>
-        <div className="w-1/3 h-full border border-gray-200">
-          <textarea
-            className="w-full h-full outline-none resize-none text-black bg-slate-700"
-            placeholder="JS"
-            onChange={handleJsChange}
-            value={jsCode}
-          ></textarea>
-        </div>
-      </section>
-      <section className="bg-white w-full h-2/3 flex">
-        <iframe
-          id="result-iframe"
-          title="Result"
-          className=" w-full"
-        ></iframe>
-      </section>
+      <main className='flex h-full overflow-hidden'>
+        <section className='w-1/3 h-full bg-gray-600'>
+          <div>
+            <button onClick={handleDownload} className='w-full bg-slate-400 hover:bg-slate-500'>Download</button>
+          </div>
+          <button onClick={handleViewClick} className='w-full text-center bg-slate-800 hover:bg-slate-900 h-10'>View</button>
+          <button onClick={handleIndexHtmlClick} className='w-full text-center bg-slate-800 hover:bg-slate-900 h-10'>Index.html</button>
+          <button onClick={handleStyleCssClick} className='w-full text-center bg-slate-800 hover:bg-slate-900 h-10'>Style.css</button>
+          <button onClick={handleScriptJsClick} className='w-full text-center bg-slate-800 hover:bg-slate-900 h-10'>Script.js</button>
+        </section>
+
+        <section className='w-2/3 h-full'>
+          {showViewSection && (
+            <section className='p-5 w-full h-full bg-white'>
+              <iframe
+                id="result-iframe"
+                title="Result"
+                className="w-full h-full"
+                ref={iframeRef}
+              ></iframe>
+            </section>
+          )}
+          {showIndexHtmlSection && (
+            <section className='w-full h-full'>
+              <textarea value={htmlCode} onChange={handleHtmlChange} className='bg-slate-500 w-full h-full'></textarea>
+            </section>
+          )}
+          {showStyleCssSection && (
+            <section className='w-full h-full'>
+              <textarea value={cssCode} onChange={handleCssChange} className='bg-slate-500 w-full h-full'></textarea>
+            </section>
+          )}
+          {showScriptJsSection && (
+            <section className='w-full h-full'>
+              <textarea value={jsCode} onChange={handleJsChange} className='bg-slate-500 w-full h-full'></textarea>
+            </section>
+          )}
+          {!showViewSection && !showIndexHtmlSection && !showStyleCssSection && !showScriptJsSection && (
+            <section className='h-full flex flex-col justify-center items-center'>
+              <h1 className='font-bold text-center'>Nothing to view</h1>
+              <p className='mt-4 text-center'>Please select a file to view</p>
+            </section>
+          )}
+        </section>
+      </main>
     </div>
   );
 }
