4cad204242263e52f5d74e8bce0b39a14d5616b8
Bashamega <adambashaahmednaji@gmail.com>
2023-06-28 07:03:16 +0300

    UPDATE - BORDERS

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,13 @@ export default function Home({state}) {
       <title>Web dev tools</title>
       <Nav></Nav>
       <div id='about' className="py-8 px-6 flex justify-center">
-        <div className="rounded-full bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
+        <div className="rounded bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
           <h1 className="text-white text-2xl font-bold">About</h1>
           <p className="text-white mt-4">WebDev Tools is a comprehensive online platform designed to empower web developers with a wide array of code samples and snippets. Whether you are a seasoned professional or just starting your journey in web development, our website provides you with a vast collection of code examples to streamline your workflow, enhance productivity, and create exceptional websites and web applications.</p>
         </div>
       </div>
       <div id='contribute' className="py-8 px-6 flex justify-center ml-5 mt-10">
-        <div className="rounded-full bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
+        <div className="rounded bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
           <h1 className="text-white text-2xl font-bold">Contribute</h1>
           <p className="text-white mt-4">
             To contribute, please visit the <a href='https://github.com/Bashamega/WebDevTools' className='text-white hover:border-b-2 border-white'>GitHub repository</a>.<br></br>
@@ -24,7 +24,7 @@ export default function Home({state}) {
         </div>
       </div>
       <div id='contributers' className="py-8 px-6 flex justify-center ml-5 mt-10">
-        <div className="rounded-full bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
+        <div className="rounded bg-gradient-to-t from-indigo-500 to-purple-600 w-3/6 p-10">
           <h1 className="text-white text-2xl font-bold m-5">Contributeres</h1>
           <div className='flex'>
             <div  className=' bg-white w-1/4 rounded p-2 m-5'>
