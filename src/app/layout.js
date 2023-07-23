057becb5c47969fd7a6dbc3a61858c5754a7e652
ratishjain <ratishjain6@gmail.com>
2023-07-23 15:42:37 +0530

    fix navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <head>
         <title>Web dev tools</title>
       </head>
