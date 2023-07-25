9732a932ad9c68fa4cd2c5fe3a744be7627b7860
ratishjain <ratishjain6@gmail.com>
2023-07-25 20:42:55 +0530

    fixed all navs

--- src/app/customizer/button/page.js
+++ src/app/customizer/button/page.js
@@ -365,9 +365,11 @@ export default function ButtonCustomizer() {
       <nav className="bg-blue-500 py-4 px-6 flex items-center justify-between">
         <a
           href="https://web-dev-tools.vercel.app/"
-          className="flex items-center border rounded p-2 hover:bg-blue-600"
+          className="flex items-center border rounded p-2 hover:bg-blue-600 mr-2"
         >
-          <h1 className="text-white text-2xl font-bold mr-2">Web Dev Tools</h1>
+          <h1 className="text-white  text-lg md:text-2xl font-bold mr-2">
+            Web Dev Tools
+          </h1>
           <p>Button Customizer</p>
         </a>
         <Search />
