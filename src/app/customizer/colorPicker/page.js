fe62c50aee48fff875411689586cc3b3ac0216db
swishyy <famodavid1@gmail.com>
2023-07-30 12:20:11 +0100

    Added functions for copying Hex and RGB values

--- src/app/customizer/colorPicker/page.js
+++ src/app/customizer/colorPicker/page.js
@@ -8,12 +8,18 @@ export default function colorPicker() {
   const [hexColor, setHexColor] = useState("#ffffff");
   const [RGBColor, setRGBCOlor] = useState("");
 
-  const handleHex = async () => {
-    console.log("Write HEX to clipboard");
+  const handleHex = () => {
+    navigator.clipboard.writeText(hexColor);
   }
 
-  const handleRGB = async () => {
-    console.log("Write RBG to clipboard");
+  const handleRGB = () => {
+
+    const toRGB = (hex) => {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? `(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
+    }
+
+    navigator.clipboard.writeText(toRGB(hexColor));
   }
 
   return (
