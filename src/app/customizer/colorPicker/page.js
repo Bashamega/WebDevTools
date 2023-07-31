e56a8f10a54e392b33d5ae6ff31249a43d0fd523
swishyy <famodavid1@gmail.com>
2023-07-31 19:43:31 +0100

    Added RGB value display

--- src/app/customizer/colorPicker/page.js
+++ src/app/customizer/colorPicker/page.js
@@ -1,25 +1,24 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from "@/app/assets/search";
 
 import { HexColorPicker } from "react-colorful"
 
 export default function colorPicker() {
   const [hexColor, setHexColor] = useState("#ffffff");
+  const [RGBColor, setRGBColor] = useState();
 
-  const handleHex = () => {
-    navigator.clipboard.writeText(hexColor);
+  const toRGB = (hex) => {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? `(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
   }
 
-  const handleRGB = () => {
+  const handleHex = () => navigator.clipboard.writeText(hexColor);
+  const handleRGB = () => navigator.clipboard.writeText(toRGB(hexColor));
 
-    const toRGB = (hex) => {
-      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? `(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
-    }
-
-    navigator.clipboard.writeText(toRGB(hexColor));
-  }
+  useEffect(() => {
+    setRGBColor(toRGB(hexColor));
+  }, [hexColor]);
 
   return (
     <main>
@@ -42,7 +41,8 @@ export default function colorPicker() {
           </span>
         </div>
         <div className="flex flex-col jusitfy-center items-center mb-8 sm:pt-[270px] xl:pr-36 xl:pt-[300px]">
-          <h1 className="font-bold text-[5.5rem] sm:text-[4rem] lg:text-7xl xl:text-8xl pb-8">{hexColor}</h1>
+          <h1 className="font-bold text-[5rem] sm:text-[4rem] lg:text-7xl xl:text-8xl pb-3">{hexColor}</h1>
+          <h1 className="font-bold text-4xl sm:text-4xl lg:text-4xl xl:text-5xl pb-8">{RGBColor}</h1>
           <button onClick={handleHex} className="bg-blue-500 hover:bg-blue-700 font-bold p-4 mb-6 transition-all">Copy Hex Code</button>
           <button onClick={handleRGB} className="bg-blue-500 hover:bg-blue-700 font-bold p-4 mb-6 transition-all">Copy RGB Code</button>
         </div>
