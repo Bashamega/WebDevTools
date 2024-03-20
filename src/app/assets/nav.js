57df24d038eaa24da57b7e99daeae3c3921a9268
Chan <chan@iPod-Classic-128GB.local>
2024-03-20 07:59:00 +0000

    added function to the search bar

--- src/app/assets/nav.js
+++ src/app/assets/nav.js
@@ -108,7 +108,7 @@ export default function Nav() {
               href="customizer/jsonGenerator"
               className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
             >
-              JsonGenerator
+              JSON Generator
             </a>
           </div>
         )}
