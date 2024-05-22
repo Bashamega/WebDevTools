1e1882978500e23b0cc6d6ad8a4937946b18702f
Younis-ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2024-05-22 19:40:14 +0300

    feat: Add Categories class for JSON Generator
    The code changes add a new file, `utils.js`, which contains the `Categories` class for the JSON Generator feature. This class provides methods for retrieving the available categories, option names within a category, and the corresponding function for generating fake data. This change completes the implementation of the JSON Generator functionality.
    
    Ref: #60

---
+++ src/app/customizer/JsonGenerator/utils.js
@@ -0,0 +1,64 @@
+import { faker } from '@faker-js/faker';
+
+
+export default class Categories {
+        #categories = {
+                Number: [
+                    { name: 'bigint', func: faker.number.bigInt},
+                    { name: 'binary', func: faker.number.binary},
+                    { name: 'float', func: faker.number.float},
+                    { name: 'hex', func: faker.number.hex},
+                    { name: 'int', func: faker.number.int},
+                    { name: 'octal', func: faker.number.octal},
+                ],
+                Lorem: [
+                    { name: 'lines', func: faker.lorem.lines},
+                    { name: 'paragraph', func: faker.lorem.paragraph},
+                    { name: 'paragraphs', func: faker.lorem.paragraphs},
+                    { name: 'sentence', func: faker.lorem.sentence},
+                    { name: 'slug', func: faker.lorem.slug},
+                    { name: 'text', func: faker.lorem.text},
+                    { name: 'word', func: faker.lorem.word},
+                    { name: 'words', func: faker.lorem.words},
+                ],
+                Person: [
+                    { name: 'bio', func: faker.person.bio},
+                    { name: 'firstName', func: faker.person.firstName},
+                    { name: 'lastName', func: faker.person.lastName},
+                    { name: 'middleName', func: faker.person.middleName},
+                    { name: 'fullName', func: faker.person.fullName},
+                    { name: 'gender', func: faker.person.gender},
+                    { name: 'jobArea', func: faker.person.jobArea},
+                    { name: 'jobDescriptor', func: faker.person.jobDescriptor},
+                    { name: 'jobTitle', func: faker.person.jobTitle},
+                    { name: 'jobType', func: faker.person.jobType},
+                ],
+                Location: [
+                    { name: 'city', func: faker.location.city},
+                    { name: 'country', func: faker.location.country},
+                    { name: 'countryCode', func: faker.location.countryCode},
+                    { name: 'county', func: faker.location.county},
+                    { name: 'direction', func: faker.location.direction},
+                    { name: 'latitude', func: faker.location.latitude},
+                    { name: 'longitude', func: faker.location.longitude},
+                    { name: 'state', func: faker.location.state},
+                    { name: 'street', func: faker.location.street},
+                    { name: 'streetAddress', func: faker.location.streetAddress},
+                    { name: 'zipCode', func: faker.location.zipCode},
+                    { name: 'buildingNumber', func: faker.location.buildingNumber},
+                    { name: 'cardinalDirection', func: faker.location.cardinalDirection},
+                ],
+            }
+
+        getECategoriesArr() {
+            return Object.keys(this.#categories);
+        }
+
+        getOptionNameArr(category) {
+            return this.#categories[category].map(option => option.name);
+        }
+
+        getFunction(category, optionName) {
+            return this.#categories[category].find(specificOption => specificOption.name === optionName).func;
+        }
+    }
