a89a4f5a2671032d973c11595854b489f12728e6
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-05-22 07:19:29 +0000

    feat: Add JSON Generator link to Nav component

--- src/app/assets/search.js
+++ src/app/assets/search.js
@@ -67,6 +67,11 @@ export default function Search() {
         name: "Mark down Editor",
         link: "https://web-dev-tools.vercel.app/MD",
       },
+      {
+        id: 8,
+        name: "Json Generator",
+        link: "https://web-dev-tools.vercel.app/customizer/JsonGenerator",
+      },
     ];
 
     const filteredData = toolList.filter(
