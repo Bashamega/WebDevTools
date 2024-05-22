ebd266d2bbc3d9a7e73936d4476d87d8c334cc60
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-05-22 07:19:37 +0000

    feat: Add JSON Generator page component
    The code changes add a new file, `page.js`, which contains the main component for the JSON Generator page. This component includes the `JsonNav`, `JsonFooter`, `CardForm`, and `Heroish` components to create the structure of the page. The purpose of this change is to provide a complete page layout for the JSON Generator feature.
    
    Ref: #60

---
+++ src/app/customizer/JsonGenerator/page.js
@@ -0,0 +1,16 @@
+"use client";
+import JsonNav from "./components/JsonNav";
+import JsonFooter from "./components/JsonFooter";
+import CardForm from "./components/CardForm";
+import Heroish from "./components/Heroish";
+export function JsonGeneratorMain(){
+    return (
+        <main>
+            <JsonNav />
+            <Heroish />
+            <CardForm />
+            <JsonFooter />
+        </main>
+)}
+
+export default JsonGeneratorMain;
