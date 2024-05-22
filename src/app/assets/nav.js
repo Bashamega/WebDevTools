9b5f9546e5c5e0dc738410c4fe47c8eb9290c5d2
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-05-22 07:19:21 +0000

    feat: Add JSON Generator link to Nav component
    The Nav component has been updated to include a link to the JSON Generator page in the dropdown menu. This will provide users with a convenient way to access the JSON Generator feature. The code has been modified to add the link and adjust the styling of the dropdown menu.
    
    Ref: #60

--- src/app/assets/nav.js
+++ src/app/assets/nav.js
@@ -112,6 +112,12 @@ export default function Nav() {
             >
               Color picker
             </a>
+            <a
+              href="customizer/JsonGenerator"
+              className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+            >
+              Json Generator
+            </a>
           </div>
         )}
       </div>
