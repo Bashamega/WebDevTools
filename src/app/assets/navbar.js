9571403ab859a3d92d2ce4bd51abdc7d52c44d2a
Bashamega <adambashaahmednaji@gmail.com>
2024-05-30 18:19:37 +0300

    Navbar component

---
+++ src/app/assets/navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Search from "./search";
+
+export function NavBar({ title }) {
+    return (
+        <nav className="bg-blue-500 py-4 px-6 flex items-center justify-between">
+            <a
+                href="https://web-dev-tools.vercel.app/"
+                className="flex items-center border rounded p-2 hover:bg-blue-600 mr-2"
+            >
+                <h1 className="text-white text-lg md:text-2xl font-bold mr-2">
+                    Web Dev Tools
+                </h1>
+                <p>{title}</p>
+            </a>
+            <Search />
+        </nav>
+    );
+}
