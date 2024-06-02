291729248f0f45409e652c26c130d23c7bc01511
Bashamega <adambashaahmednaji@gmail.com>
2024-06-02 11:14:53 +0300

    Updated Links

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -34,9 +34,14 @@ export default function Nav() {
   return (
     <nav className="bg-blue-500 py-2 px-4 flex items-center justify-around gap-1 w-full relative">
       <div className="flex flex-0 items-center flex-shrink">
-        <div className="text-white text-[0.7rem] font-bold sm:text-lg md:text-2xl sm:font-bold">
-          <p>Web Dev Tools</p>
-        </div>
+        <a
+          href="https://web-dev-tools.vercel.app/"
+          className="flex items-center border rounded p-2 hover:bg-blue-600 mr-2"
+        >
+          <h1 className="text-white text-lg md:text-2xl font-bold mr-2">
+            Web Dev Tools
+          </h1>
+        </a>
         &emsp;
         <div className="hidden lg:block mr-2">
           <Search />
@@ -51,9 +56,8 @@ export default function Nav() {
           <FaTools fontSize={20} className="mr-2" />
           Customizer tools
           <svg
-            className={`${
-              isDropdownOpen ? "transform rotate-180" : ""
-            } inline-block ml-[2px] w-[12px] h-4`}
+            className={`${isDropdownOpen ? "transform rotate-180" : ""
+              } inline-block ml-[2px] w-[12px] h-4`}
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -143,14 +147,14 @@ export default function Nav() {
           </p>
         </a>
         <a
-          href="#about"
+          href="/about"
           className="text-white font-bold text-[0.6rem]  sm:text-sm p-2 hover:bg-blue-700 transition-all duration-700 rounded-lg flex items-center justify-center gap-2"
         >
           <FaInfo fontSize={15} />
           About
         </a>
         <a
-          href="https://github.com/Bashamega/WebDevTools"
+          href="/contribute"
           className="text-white font-bold text-[0.6rem]  sm:text-sm  p-2 hover:bg-blue-700 transition-all duration-700 rounded-lg flex items-center justify-center gap-2"
         >
           <IoMdGitPullRequest fontSize={20} />
@@ -160,11 +164,10 @@ export default function Nav() {
           <SearchIcon className="text-white" onClick={searchToggle} />
         </button>
         <div
-          className={`absolute w-full h-full flex  items-center  bg-blue-500 ${
-            toggle
+          className={`absolute w-full h-full flex  items-center  bg-blue-500 ${toggle
               ? "left-0 duration-300 ease-in"
               : "left-[100%] duration-300 ease-in"
-          } `}
+            } `}
         >
           <div className="flex flex-1 items-center text-white justify-center relative">
             <ArrowBackIcon
