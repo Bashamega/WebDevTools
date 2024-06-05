6a4a95ebfade8b98aa3230a25330624a0036c45e
Bashamega <adambashaahmednaji@gmail.com>
2024-06-05 12:28:15 +0300

    Update

--- src/app/snippet/page.jsx
+++ src/app/snippet/page.jsx
@@ -2,13 +2,16 @@
 import React, { useState } from "react";
 
 import { NavBar } from "@/app/components/navbar";
+import Footer from "../components/Footer";
 
 export default function ButtonCustomizer() {
-
+  const [articles, setarticles] = useState([])
   return (
-    <main className="h-screen flex flex-col gap-10">
+    <main className="h-screen w-screen flex flex-col gap-10">
       <NavBar title={"Snippets"}/>
-      
+      <div className="flex items-center justify-center w-full h-full">
+        <Footer/>
+      </div>
     </main>
   );
 }
