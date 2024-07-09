2ce69f7c4e35bcfac072ef8ea4a53cb023096ca7
ANNU KUMARI <anuk35168@gmail.com>
2024-07-09 11:49:06 +0530

    commit page resovled

--- src/app/codeedit/page.jsx
+++ src/app/codeedit/page.jsx
@@ -1,25 +1,3 @@
-// "use client";
-
-// import React, { useState } from "react";
-// import { NavBar } from "@/app/components/navbar";
-// import Editor from "./components/Editor";
-
-// const Page = () => {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-
-//   const toggleTheme = () => {
-//     setIsDarkMode(!isDarkMode);
-//   };
-
-//   return (
-//     <div className={`max-h-[100vh] overflow-hidden ${isDarkMode ? "bg-gray-900 text-gray-400" : "bg-gray-100 text-gray-800"}`}>
-//       <NavBar title={"Code Editor"} isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-//       <Editor isDarkMode={isDarkMode} />
-//     </div>
-//   );
-// };
-
-// export default Page;
 "use client";
 
 import React, { useState } from "react";
