4f9da08dae45fb9ef1941b7aeaf511bd45f7615c
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2024-07-19 13:55:59 +0300

    Update src/app/customizer/Readme-generator/page.jsx

--- src/app/customizer/Readme-generator/page.jsx
+++ src/app/customizer/Readme-generator/page.jsx
@@ -114,7 +114,7 @@ ${license}
             className={`w-full h-full ${isDarkMode ? "bg-gray-800 text-white" : "bg-white text-black"}`}
             title="Parsed Markdown"
             srcDoc={`<!DOCTYPE html><html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css"><style>body { margin: 0; padding: 16px; background-color: ${isDarkMode ? "#1a202c" : "white"}; color: ${isDarkMode ? "white" : "black"}; }</style></head><body class="markdown-body">${snarkdown(
-              markdown,
+              markdown(),
             )}</body></html>`}
           />
       </div>
