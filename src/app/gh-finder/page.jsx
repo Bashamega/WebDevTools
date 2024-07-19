29a0a8ef75da6618b8b6a7dd08f264437fcfcfaf
Bashamega <adambashaahmednaji@gmail.com>
2024-07-19 18:17:57 +0300

    Preview change

--- src/app/gh-finder/page.jsx
+++ src/app/gh-finder/page.jsx
@@ -77,49 +77,62 @@ export default function GhFinder() {
       <div className=" ml-[25%] w-1/2">
         {data ? (
           data.map((item, index) => (
-            <div
-              key={item.id}
-              className="bg-slate-500 flex flex-col justify-center items-center rounded mb-5 pl-5"
-            >
-              <Link
-                href={item.html_url}
-                className="text-white font-bold text-3xl hover:underline text-center"
-              >
-                {item.title}
-              </Link>
-              <div className="flex flex-wrap mt-2 items-center justify-center">
-                {item.labels && item.labels.length > 0 ? (
-                  item.labels.map((label) => {
-                    const isDark = isDarkColor(label.color);
-                    const textColor = isDark ? "text-white" : "text-gray-800";
-
-                    return (
-                      <p
-                        key={label.id}
-                        className={`bg-gray-300 ${textColor} px-2 py-1 rounded mr-2 mb-2 cursor-pointer`}
-                        style={{ backgroundColor: `#${label.color}` }}
-                      >
-                        {label.name}
-                      </p>
-                    );
-                  })
-                ) : (
-                  <p className="text-white">No labels</p>
-                )}
-              </div>
-              <div className="flex items-center">
-                <p className="text-white">Created by:</p>
+            <div key={item.id} className="bg-slate-500 rounded mb-5 pl-5 flex w-full">
+              <div className="w-2/3 overflow-hidden">
                 <Link
-                  href={item.user.html_url}
-                  className="flex items-center text-white ml-5"
+                  href={item.html_url}
+                  className="text-white font-bold text-3xl hover:underline truncate block"
                 >
-                  <img
-                    src={item.user.avatar_url}
-                    className="rounded-full w-6 h-6 mr-2"
-                    alt="User Avatar"
-                  />
-                  <p className="text-white">{item.user.login}</p>
+                  {item.title}
                 </Link>
+                <div className="flex flex-wrap mt-2 items-center w-2/3 gap-1 max-h-[50px] overflow-auto">
+                  {item.labels && item.labels.length > 0 ? (
+                    item.labels.map((label) => {
+                      const isDark = isDarkColor(label.color);
+                      const textColor = isDark ? "text-white" : "text-gray-800";
+
+                      return (
+                        <p
+                          key={label.id}
+                          className={`bg-gray-300 ${textColor}   px-2 py-1 rounded mr-2 mb-2 cursor-pointer truncate w-[calc(30%)]`}
+                          style={{ backgroundColor: `#${label.color}` }}
+                        >
+                          {label.name}
+                        </p>
+                      );
+                    })
+                  ) : (
+                    <p className="text-white">No labels</p>
+                  )}
+                </div>
+                <div className="flex items-center">
+                  <p className="text-white">Created by:</p>
+                  <Link
+                    href={item.user.html_url}
+                    className="flex items-center text-white ml-5"
+                  >
+                    <img
+                      src={item.user.avatar_url}
+                      className="rounded-full w-6 h-6 mr-2"
+                      alt="User Avatar"
+                    />
+                    <p className="text-white">{item.user.login}</p>
+                  </Link>
+                </div>
+              </div>
+              <div className="w-1/3 p-5">
+                <div className="text-white flex overflow-hidden items-center justify-between">
+                  <p>Assignees: </p>
+                  {item.assignees.length > 0 ? (
+                    <div className="flex -space-x-4">
+                      {item.assignees.map(assignee=>(
+                        <img alt={assignee.login} src={assignee.avatar_url} className=" rounded-full w-5 h-5 border border-white"/>
+                      ))}
+                    </div>
+                  ) : (
+                    <p>0</p>
+                  )}
+                </div>
               </div>
             </div>
           ))
