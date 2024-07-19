d91c472a7e4aa7cbaa0302ebeb4a3af35713c162
Bashamega <adambashaahmednaji@gmail.com>
2024-07-19 13:46:04 +0300

    Responsive

--- src/app/gh-finder/page.jsx
+++ src/app/gh-finder/page.jsx
@@ -17,11 +17,11 @@ export default function GhFinder() {
         toggleTheme={toggleTheme}
       />
       <div className=" mt-10 flex w-screen justify-center">
-        <header className=" w-2/3">
+        <header className=" lg:w-2/3">
           <h1 className="relative z-10 font-sans text-lg font-bold text-center text-transparent md:text-7xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600">
             Github Issue Finder
           </h1>
-          <div className=" flex justify-between w-1/2 mx-[25%] my-5">
+          <div className=" flex justify-between w-full lg:w-1/2 lg:mx-[25%] my-5">
             <button className={"hover:bg-blue-800 transition-colors min-w-1/3 duration-100 p-5 rounded-full hover:text-white " + (selected == 1&& "bg-blue-600  text-white")} onClick={()=>setSelected(1)}>Web Dev Tools Issues</button>
             <button className={"hover:bg-blue-800 transition-colors w-1/3 duration-100 p-5 rounded-full hover:text-white " + (selected == 2&& "bg-blue-600  text-white")} onClick={()=>setSelected(2)}>Github</button>
           </div>
