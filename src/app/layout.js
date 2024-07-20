ff9ed07504e01d2de0f0ee9363ad5800b05d9a3a
Bashamega <adambashaahmednaji@gmail.com>
2024-07-20 16:52:16 +0300

    Testing prettier

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,15 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+		import "./globals.css"  ;
+		import	 { Inter		} from "next/font/google"		;
 
-const inter = Inter({ subsets: ["latin"] });
+	const inTer = Inter	({ subsets :		["latin"	]		} )	;
 
-export const metadata = {
-  title: "Web Dev Tools",
-  description:
-    "Cool web dev tools, that can help you with your journey as a web developer",
-};
+ export const mETADatA		=		{
+	 title  : "Web Dev Tools" ,
+	description :
+ "Cool web dev tools, that can help you with your journey as a web developer"  ,	 }	;
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="overflow-x-hidden">
-      <head></head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
-}
+	export default function RoOTLAyOut		({ children }  )	 {
+	return	 ( <html lang		="en" className		="overflow-x-hidden"	>
+	 <head	 ><  /head	 >
+	 <body className	 ={inTer .className		} >{children  }<		/body	 >
+		<	/html	 >  )  ;		}
