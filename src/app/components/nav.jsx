64a44fd07d805f7d23e20c3820fe293a0f0ebef4
Aman <dv451197@gmail.com>
2024-07-21 22:28:37 +0530

    revert changes

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -8,7 +8,6 @@ import { FaTools, FaCode, FaMarkdown, FaInfo } from "react-icons/fa";
 import { IoMdGitPullRequest } from "react-icons/io";
 import Link from "next/link";
 import Switch from "@mui/material/Switch";
-import tools from "@/db/tools.json";
 
 export default function Nav({ isDarkMode, toggleTheme }) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -109,34 +108,19 @@ export default function Nav({ isDarkMode, toggleTheme }) {
             ref={dropdownRef}
             className={`absolute right-0 mt-2 py-2 ${
               isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"
-            } rounded shadow-lg w-48 overflow-y-scroll max-h-[500px]`}
+            } rounded shadow-lg w-40`}
             style={{ zIndex: 100 }}
           >
-            {
-              tools.map((tool) => (
-                <>
-                <Link
-                key={tool.id}
-                href={tool.link}
-                className={`block px-4 py-2 hover:${
-                  isDarkMode ? "bg-gray-800 text-white" : "bg-gray-200"
-                }`}
-              >
-               {tool.name}
-              </Link>
-              <hr />
-              </>
-              ))
-            }
-            {/* <Link
+            <Link
               href="/customizer/box-shadow-generator"
               className={`block px-4 py-2 hover:${
                 isDarkMode ? "bg-gray-800 text-white" : "bg-gray-200"
               }`}
             >
               Box Shadow Generator
-            </Link> */}
-            {/* <Link
+            </Link>
+            <hr />
+            <Link
               href="/customizer/gradient-generator"
               className={`block px-4 py-2 hover:${
                 isDarkMode ? "bg-gray-800 text-white" : "bg-gray-200"
@@ -208,14 +192,6 @@ export default function Nav({ isDarkMode, toggleTheme }) {
             >
               Readme Generator
             </Link>
-            <Link
-              href="/asciiDoc"
-              className={`block px-4 py-2 hover:${
-                isDarkMode ? "bg-gray-800 text-white" : "bg-gray-200"
-              }`}
-            >
-              AsciiDoc Editor
-            </Link> */}
           </div>
         )}
       </div>
