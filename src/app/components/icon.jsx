9db62ecd254329a4f3031bdda126060d6bce6fae
HarshitT00 <108449687+HarshitT00@users.noreply.github.com>
2024-07-22 13:14:34 +0530

    Update icon.jsx with a default export

--- src/app/components/icon.jsx
+++ src/app/components/icon.jsx
@@ -41,3 +41,10 @@ export const MoonIcon = ({ className = "" }) => (
     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
   </svg>
 );
+
+const Icons = {
+  SunIcon,
+  MoonIcon
+};
+
+export default Icons;
