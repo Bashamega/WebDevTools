5d11c2f31e1570345e11c105b02445af63004a18
Bashamega <adambashaahmednaji@gmail.com>
2024-07-23 11:02:00 +0300

    New Page

---
+++ src/app/convert/html-jsx/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import { NavBar } from "@/app/components/navbar";
+
+export default function HTML_JSX() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+    localStorage.setItem("theme", JSON.stringify(newTheme));
+  };
+  return (
+    <main
+      className={`h-screen overflow-auto ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"}`}
+    >
+      <NavBar title={"HTML to JSX"} />
+    </main>
+  );
+}
