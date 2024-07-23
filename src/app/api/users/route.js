cd12f18dddbf17412d5f3c6c41231d22c2c354e8
michael seaman <mseaman26@gmail.com>
2024-07-22 19:35:03 -0700

    endpoints for users and posts

---
+++ src/app/api/users/route.js
@@ -0,0 +1,20 @@
+import users from '@/db/users.json'
+import { NextResponse } from 'next/server';
+
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get('page'));
+    if(isNaN(page) || page < 1) {
+        return NextResponse.json({ error: "Invalid page number" }, { status: 400 });
+    }
+    const usersPerPage = 10;
+    const startIndex = (page - 1) * usersPerPage;
+
+    if (startIndex < 0 || startIndex >= users.length) {
+        return NextResponse.json({ error: "Page not found" }, { status: 404 });
+    }
+
+    const paginatedUsers = users.slice(startIndex, startIndex + usersPerPage);
+    return NextResponse.json(paginatedUsers, { status: 200 });
+}
