ffce3be5c60ede5c96196992198e5c4f2d5e7c41
Oslonline <Oslo418@yandex.ru>
2024-07-24 18:33:31 +0200

    fix: minor styling changes: adjust shadow & remove extra space to adjust height of footer on mobile

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -14,7 +14,7 @@ const Footer = ({ isDarkMode }) => {
 
   return (
     <footer
-      className={`w-[26rem] md:w-[40rem] max-w-full rounded-lg shadow-lg m-4 ${isDarkMode ? "bg-gray-800 text-white" : "bg-white text-black"} fixed bottom-0`}
+      className={`w-[26rem] md:w-[40rem] max-w-full rounded-lg shadow-md m-4 ${isDarkMode ? "bg-gray-800 text-white" : "bg-white text-black"} fixed bottom-0`}
     >
       <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-center flex-col items-center md:flex-row md:justify-between">
         <span
@@ -26,7 +26,6 @@ const Footer = ({ isDarkMode }) => {
           </Link>
           . All Rights Reserved.
         </span>{" "}
-        &emsp;
         <ul
           className={`flex flex-wrap items-center mt-3 text-sm font-medium ${isDarkMode ? "text-gray-400" : "text-gray-500"} sm:mt-0`}
         >
