6c371a2f2b13311a63bfe6d7a4c08e56d046064c
Nicolás Hernán Silva <83423412+Koppeks@users.noreply.github.com>
2024-07-26 13:19:19 -0300

    Update src/app/convert/css-scss/components/SnippetBox.jsx
    Co-authored-by: Adam Basha <110662505+Bashamega@users.noreply.github.com>

--- src/app/convert/css-scss/components/SnippetBox.jsx
+++ src/app/convert/css-scss/components/SnippetBox.jsx
@@ -9,7 +9,7 @@ export default function SnippetBox({
   return (
     <textarea
       onChange={(e) => setSnippet(e.target.value)}
-      className={`w-full shadow-inner p-4 border-0 ${isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
+      className={`resize-none w-full shadow-inner p-4 border-0 ${isDarkMode ? "bg-gray-700 text-white" : "bg-white text-black"}`}
       placeholder={`Enter your ${from} here`}
       rows="10"
       value={snippet}
