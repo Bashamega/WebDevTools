053e3142f618a3de909495289803c7b15a304eb0
Aman <dv451197@gmail.com>
2024-07-29 12:06:15 +0530

    added default style and layout

--- src/app/resume-builder/page.jsx
+++ src/app/resume-builder/page.jsx
@@ -3,20 +3,41 @@
 import { useState } from "react";
 import Form from "./Form";
 import Preview from "./Preview";
+import { NavBar } from "../components/navbar";
 
 const Home = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const [formData, setFormData] = useState(null);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   const handleFormSubmit = (data) => {
     setFormData(data);
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl mb-4">Resume Builder</h1>
-      <Form onSubmit={handleFormSubmit} />
-      {formData && <Preview data={formData} />}
-    </div>
+    <main
+      className={`${
+        isDarkMode ? "bg-gray-900 text-gray-400" : "bg-gray-100 text-gray-500"
+      } min-h-screen w-full pb-2`}
+    >
+      <NavBar
+        title={"GitHub Issue Finder"}
+        isDarkMode={isDarkMode}
+        toggleTheme={toggleTheme}
+      />
+      <div className="lg:w-2/3 mx-auto p-4 my-4">
+        <h1 className="relative z-10 font-sans text-lg font-bold text-center text-transparent md:text-7xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600">
+          Resume Builder
+        </h1>
+        <div className="w-full mt-4">
+          <Form onSubmit={handleFormSubmit} />
+          {formData && <Preview data={formData} />}
+        </div>
+      </div>
+    </main>
   );
 };
 
