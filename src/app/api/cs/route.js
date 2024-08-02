24eb99c2389a8b2a080768f2ae09791d99575df7
Bashamega <adambashaahmednaji@gmail.com>
2024-08-02 14:06:52 +0300

    Categories

---
+++ src/app/api/cs/route.js
@@ -0,0 +1,8 @@
+import categoris from "@/db/codesnippets/categories.json";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {  
+  const response = NextResponse.json(categoris, { status: 200 });
+  response.headers.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
+  return response;
+}
