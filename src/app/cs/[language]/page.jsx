32254f14b83c34f7c0f5eb9778732ff58a868d3a
Bashamega <adambashaahmednaji@gmail.com>
2024-08-06 13:34:08 +0300

    chore: make sure the category exists

--- src/app/cs/[language]/page.jsx
+++ src/app/cs/[language]/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 import { useState, useEffect } from "react";
 import { NavBar } from "../../components/navbar";
+import languages from "@/db/codesnippets/categories.json";
 import Link from "next/link";
+import NotFound from "@/app/not-found";
 
-export default function CodingSnippetsTopic({params}) {
+export default function CodingSnippetsTopic({ params }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [notfound, setNotfound] = useState(false);
 
   // Toggle theme and save preference in localStorage
   const toggleTheme = () => {
@@ -19,27 +22,45 @@ export default function CodingSnippetsTopic({params}) {
       setIsDarkMode(storedTheme);
     }
   }, []);
-  const title = (text)=>{
-    return text.toLowerCase().replace(/\b\w/g, char => char.toUpperCase());
-  }
+
+  useEffect(() => {
+    const languageExists = languages.languages.some((language) => {
+      return language.name.toLowerCase() === params.language.toLowerCase();
+    });
+    if (!languageExists) {
+      setNotfound(true);
+    }
+  }, [languages.languages, params.language]);
+
+  const title = (text) => {
+    return text.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+  };
 
   return (
-    <main
-      className={`h-screen overflow-auto ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"}`}
-    >
-      <NavBar
-        title={"Coding Snippets | " + title(params.language)}
-        isDarkMode={isDarkMode}
-        toggleTheme={toggleTheme}
-      />
-      <div className="flex flex-col justify-center items-center h-fullpt-52 px-5">
-        <section className="text-center p-10 rounded-lg">
-          <h1 className="text-4xl font-bold mb-4">{title(params.language)} Snippets</h1>
-          <p className="text-lg">A free collection of {title(params.language)} snippets</p>
-
-          
-        </section>
-      </div>
-    </main>
+    <>
+      {notfound ? (
+        <NotFound />
+      ) : (
+        <main
+          className={`h-screen overflow-auto ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"}`}
+        >
+          <NavBar
+            title={"Coding Snippets / " + title(params.language)}
+            isDarkMode={isDarkMode}
+            toggleTheme={toggleTheme}
+          />
+          <div className="flex flex-col justify-center items-center h-fullpt-52 px-5">
+            <section className="text-center p-10 rounded-lg">
+              <h1 className="text-4xl font-bold mb-4">
+                {title(params.language)} Snippets
+              </h1>
+              <p className="text-lg">
+                A free collection of {title(params.language)} snippets
+              </p>
+            </section>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
