a682db8ae2d6a146d11d047e86cf75812ad3f278
Bashamega <adambashaahmednaji@gmail.com>
2024-08-08 12:15:10 +0300

    chore: fix spelling

--- src/app/api/cs/route.js
+++ src/app/api/cs/route.js
@@ -1,8 +1,8 @@
-import categoris from "@/db/codesnippets/categories.json";
+import categories from "@/db/codesnippets/categories.json";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const response = NextResponse.json(categoris, { status: 200 });
+  const response = NextResponse.json(categories, { status: 200 });
   response.headers.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
   return response;
 }
