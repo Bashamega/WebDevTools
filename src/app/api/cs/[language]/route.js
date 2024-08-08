29ed72fb528c44c80239011c01f36bae2ab3bc8e
Bashamega <adambashaahmednaji@gmail.com>
2024-08-08 12:41:03 +0300

    Fetch category

--- src/app/api/cs/[language]/route.js
+++ src/app/api/cs/[language]/route.js
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 import languages from "@/db/codesnippets/categories.json";
 
 export async function GET(request, { params }) {
-  const { language } = params; // Extract category and language from params
+  const { language } = params; // Extract language from params
+  const encodedLanguage = encodeURIComponent(language.toLowerCase());
 
   // Check if the language exists in the languages list
   const languageExists = languages.languages.some((lang) => {
-    return lang.name.toLowerCase() === language.toLowerCase();
+    return encodeURIComponent(lang.name.toLowerCase()) === encodedLanguage;
   });
 
   // If the language does not exist, return a 404 response
@@ -15,12 +16,19 @@ export async function GET(request, { params }) {
     response.headers.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
     return response;
   } else {
-    // If the language exists, return a success response
-    const response = NextResponse.json(
-      { text: "hey", category: language },
-      { status: 200 }
-    );
-    response.headers.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
-    return response;
+    try {
+      const content = require(`@/db/codesnippets/posts/${language}/content.json`);
+      // If the language exists, return a success response
+      const response = NextResponse.json(
+        { category: language, content: content },
+        { status: 200 }
+      );
+      response.headers.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
+      return response;
+    } catch (error) {
+      console.error(`Error loading content for language: ${language}`, error);
+      const response = NextResponse.json({ error: "Internal Server Error", message:error }, { status: 500 });
+      return response;
+    }
   }
 }
