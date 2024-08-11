11914c496d9cd936ab1dea2e019a6d525c04abc5
Bashamega <adambashaahmednaji@gmail.com>
2024-08-11 11:25:06 +0300

    feat: fetcher tool

---
+++ src/app/fetcher/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState, useEffect } from "react";
+import { NavBar } from "../components/navbar";
+
+export default function CodingSnippets() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Toggle theme and save preference in localStorage
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+    localStorage.setItem("theme", JSON.stringify(newTheme));
+  };
+
+  useEffect(() => {
+    const storedTheme = JSON.parse(localStorage.getItem("theme"));
+    if (storedTheme !== null) {
+      setIsDarkMode(storedTheme);
+    }
+  }, []);
+
+  return (
+    <main
+      className={`h-screen overflow-auto ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"}`}
+    >
+      <NavBar
+        title={"Fetcher"}
+        isDarkMode={isDarkMode}
+        toggleTheme={toggleTheme}
+      />
+      
+    </main>
+  );
+}
