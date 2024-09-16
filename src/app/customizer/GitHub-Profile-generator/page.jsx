84ae36933d044448dad2b8a0921dcaa6cc823d68
Bashamega <adambashaahmednaji@gmail.com>
2024-09-16 14:39:26 +0300

    patch: ISSUE #315: 2

--- src/app/customizer/GitHub-Profile-generator/page.jsx
+++ src/app/customizer/GitHub-Profile-generator/page.jsx
@@ -593,7 +593,7 @@ export default function ButtonCustomizer() {
               type="text"
               name="linkedin"
               placeholder="LinkedIn URL"
-              value={formValues.linkedin}
+              value={encodeURIComponent(formValues.linkedin)}
               onChange={handleChange}
               className={`${
                 isDarkMode
