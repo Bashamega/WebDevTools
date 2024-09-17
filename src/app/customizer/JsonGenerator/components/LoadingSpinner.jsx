418c9e473de24fc37364adfbaad2cffb3e91db3d
Bashamega <adambashaahmednaji@gmail.com>
2024-09-17 13:53:33 +0300

    patch: missing import

--- src/app/customizer/JsonGenerator/components/LoadingSpinner.jsx
+++ src/app/customizer/JsonGenerator/components/LoadingSpinner.jsx
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 export const LoadingSpinner = () => (
   <div className="flex items-center justify-center w-full h-full">
     <Box sx={{ display: "flex" }}>
