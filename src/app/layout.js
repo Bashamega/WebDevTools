eb59acb40bd66e14cab72ec61eaee36938a4a1bf
Bashamega <adambashaahmednaji@gmail.com>
2024-10-18 14:58:43 +0300

    fix: Casing & Meta details

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,11 @@
-"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import Banner from "free-ps-banner";
 
-const inTer = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-export const mETADatA = {
+export const metadata = {
   title: "Web Dev Tools",
   description:
     "Cool web dev tools, that can help you with your journey as a web developer",
@@ -18,7 +17,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body className={inTer.className}>
+      <body className={inter.className}>
         <div id="banner" className="sticky top-0 z-50">
           <Banner showButton={false} className="mb-7" />
         </div>
